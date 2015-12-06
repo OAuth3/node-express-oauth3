38cd2c6d0a3ef5a83b75d1ae7ce28dd32f3c29fd
AJ ONeal <awesome@coolaj86.com>
2015-12-06 09:18:35 +0000

    accesstoken-ception

--- walnut.js
+++ walnut.js
@@ -78,43 +78,70 @@ module.exports.create = function (conf, deps, app) {
 
   TokenSigner = {
     // The purpose of this is essentially to establish a session
-    signAsync: function (data) {
-      // "https://facebook.com:765/yoyoy/?#"
-      var abnormalHostname = (data.referer || '');
-      var abnormalHostname2 = (data.host || '');
-      var appname = abnormalHostname.replace(/(https?:\/\/)?([^:\/]+)(:\d+)?(\/[^#\?]+)?.*/, '$2$4').replace(/\/$/, '');
-      var appname2 = abnormalHostname2.replace(/(https?:\/\/)?([^:\/]+)(:\d+)?(\/[^#\?]+)?.*/, '$2$4').replace(/\/$/, '');
-      var issuedAt = Math.floor(Date.now() / 1000);
-      var privkey;
+    signAsync: function (meta, params) {
+      /* meta = { referer, host, scope, providerUri, browserState } */
+      // https://tools.ietf.org/html/rfc6749#section-4.2.2
+      /* params = { access_token, refresh_token, scope, state, token_type, expires_in, expires_at, expires (in) } */
+
+      var crypto = require('crypto');
+      // "https://facebook.com:765/yoyoy/?#" -> facebook.com/yoyoy
+      var re = /(https?:\/\/)?([^:\/]+)(:\d+)?(\/[^#\?]+)?.*/;
       // we probably want to drop the port and any trailing junk after the last '/'
+      var appname = (meta.referer || '').replace(re, '$2$4').replace(/\/$/, '');
+      var appname2 = (meta.host || '').replace(re, '$2$4').replace(/\/$/, '');
+      var issuedAt = Math.floor(Date.now() / 1000);
+      var audience = appname;
+      var expiresIn = (1 * 60 * 60);
+      var expiresAt = Math.floor(new Date(Date.now() + (expiresIn * 1000)).valueOf() / 1000);
+      var privkey;
+      var issuer;
 
       if (conf.keys[appname]) {
         privkey = conf.keys[appname].privkey;
+        issuer = appname;
       } else {
         if (conf.keys[appname2]) {
           privkey = conf.keys[appname2].privkey;
+          issuer = appname2;
         } else {
           return PromiseA.reject({
-            message: "'" + appname + "' is not configured for signing with a private key"
+            message: "neither '" + appname + "' nor '" + appname + "' is configured for signing with a private key"
           , code: "E_NO_PRIVATE_KEY"
           });
         }
       }
 
-      // NOTE data.scope is available for reference
       // TODO perform login and attack acs to this session token
+
+      // https://tools.ietf.org/html/draft-ietf-oauth-json-web-token-32#section-4.1
+      // https://openid.net/specs/draft-jones-json-web-token-07.html
       // see org.oauth3.provider/oauthclient-microservice/lib/oauth3orize.js
-      var tok = jwt.sign({
-        accessToken: data.params.access_token
-      , consumer: appname // aud?
-      , refreshToken: data.params.refresh_token
-      , providerUri: data.providerUri
-      , params: data.params // expirey?
-      , iat: issuedAt
-      //, exp: ''
+      /* jwt = { jti, iss, aud, sub, typ, iat, exp, data } */
+      var accessToken = jwt.sign({
+        jti: crypto.randomBytes(16).toString('hex') // prevent replays
+      , iss: issuer               // the audience must trust the public keys of the issuer
+      , aud: audience             // the issuer may have multiple audiences and therefore must specify (for the audience's sake)
+      //, prn: meta.providerUri     // https://openid.net/specs/draft-jones-json-web-token-07.html
+      , sub: meta.providerUri     // rfc7519 for prn - the subject (principle) - in this case is the 3rd party login
+      , typ: 'credentials'        // how to know what
+      , iat: issuedAt             // IntDate
+      , exp: expiresAt            // IntDate
+      , data: params              // non-spec, application specific
       }, privkey, { algorithm: 'RS256' });
 
-      return PromiseA.resolve(tok);
+      // https://tools.ietf.org/html/rfc6749#section-4.2.2
+      return PromiseA.resolve({
+      // The access token should have a lot of data (prevent db lookups)
+        access_token: accessToken
+      // The refresh token will require a database lookup (and check if the user is still allowed - PCI/SOX compliance)
+      , refresh_token: '' // refreshToken
+      // expires_at refers to accessToken, but since it's jwt
+      , expires_at: expiresAt
+      , expires_in: expiresIn
+        // TODO declare what is granted with this token
+      , scope: undefined
+      , token_type: 'bearer'
+      });
     }
   };
 
