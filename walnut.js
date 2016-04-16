b7e72263727850e5e0e14f226078995373f75829
AJ ONeal <awesome@coolaj86.com>
2016-04-16 01:24:47 -0400

    move towards automatic registration

--- walnut.js
+++ walnut.js
@@ -9,31 +9,7 @@ module.exports.create = function (xconfx, deps, app) {
   var DirectiveStore = require('./lib/directive-store').create(things.DirStore, things.RegStore);
   var TokenSigner = require('./lib/token-signer').create(things.DirStore, things.KeyStore);
 
-  var options;
-
-  //
-  // Example for getting and setting registration information
-  // (OAuth3 can automatically register app id / app secret)
-  //
-
-  //
-  // Automatic Registration
-  //
-  // For services that support automatic registration,
-  // tell the service your security policy.
-  //
-  // DRAFT (this is not yet spec'd)
-  /*
-  function getRegistrationOptions(providerUri) {
-    return {
-      allowed_domains: [ "https://awesome.com", "https://partner-site.com" ]
-    , allowed_cnames: [ "internal.example.com", "external.example.com" ]
-    , allowed_redirects: [ "https://awesome.com/oauth3.html", "https://api.awesome.com/oauth3/" ]
-    };
-  }
-  */
-
-  options = {
+  var options = {
     // walnut handles '/api/org.oauth3.consumer' as '/'
     // but will also offer it as '/api' and '/api/org.oauth3.consumer'
     // for compatibility
@@ -46,8 +22,7 @@ module.exports.create = function (xconfx, deps, app) {
   //, authorizationCodeCallbackUrl: /*options.domain +*/ '{{host}}/api/org.oauth3.consumer/authorization_code_callback'
   };
 
-
-  // TODO clean-up, 'things' should be passed in
+  // TODO clean-up, maybe 'things' should be passed in ?
 
   return OAuth3.create(xconfx, app, TokenSigner, DirectiveStore, kvStore, options);
 };
